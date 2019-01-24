d2e04085cb4e503a5da1e65dbd40fffdb8940fe7
Emile <emile_berteloot@hotmail.com>
2019-01-24 21:16:30 +0100

    Rework, GUI is for later. Given artist or playlist, get album covers.

--- spotipyApp.py
+++ spotipyApp.py
@@ -1,140 +1,49 @@
 import spotipy
 import spotipy.util as util
 import logging
-import io
-import urllib
-from PIL import ImageTk
-from PIL import Image as imImage
-from Tkinter import *
-from helpers import *
+import argparse
+import sys
 
+logging.basicConfig(level=logging.INFO)
 
-def showresults(window, results):
+parser = argparse.ArgumentParser(description="Input mode: artist, playlist, etc.")
+parser.add_argument("-s", "--source", help="The source from which you want to take album covers. Options are 'artist' and 'playlist'",
+					required=True)
+parser.add_argument("-u", "--uri", help="The URI of the artist or playlist for which you want to collect the album covers",
+					required=True)
+args = parser.parse_args()
 
-	res = QueryResult(results)
-	currAlbumWidgets = []
+if args.source != "artist" and args.source != "playlist":
+	logging.error("Not a valid input source. Choose 'artist' or 'playlist'.")
+	sys.exit(0)
 
-	lbl = Label(window, text=res.parseInfo())
-	lbl.configure(bg="grey")
-	lbl.grid(column=0, row=4)
-	currAlbumWidgets.append(lbl)
+source = args.source
+uri = args.uri
 
-	id = 0
-	for name in res.tracks["name"]:
-		id += 1
-		lbltext = "%d: %s" % (id, name)
-		label = Label(window, text=lbltext)
-		label.configure(bg="grey")
-		label.grid(sticky=W, column=0, row=id + 4)
-		currAlbumWidgets.append(label)
+if source not in uri:
+	logging.error("URI does not match source: needs to be an artist or a playlist.")
+	sys.exit(0)
 
-	logging.info("URL is %s" % res.coverurl)
-	raw_data = urllib.urlopen(res.coverurl).read()
-	im = imImage.open(io.BytesIO(raw_data))
-	im = im.resize((350, 350), imImage.ANTIALIAS)
-	image = ImageTk.PhotoImage(im)
-	imageLabel = Label(window, image=image)
-	imageLabel.image = image
-	imageLabel.grid(column=0, row=id + 5)
-	currAlbumWidgets.append(imageLabel)
+client_id = "580cb72fd1364d10aa8bd0f8d4bf5c32"
+client_secret = "fa391051c65748b7b7b35d7d6d4faa93"
+redirect_uri = "https://www.google.be/"
+scope = 'user-library-read'
 
-	return currAlbumWidgets
+username = "1136425634"
 
-class App:
+token = util.prompt_for_user_token(username, scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+if token:
+	sp = spotipy.Spotify(auth=token)
+else:
+	logging.ERROR("Cannot login with this user account")
+	sys.exit(0)
 
-	def __init__(self, master):
-		self.client_id = "580cb72fd1364d10aa8bd0f8d4bf5c32"
-		self.client_secret = "fa391051c65748b7b7b35d7d6d4faa93"
-		self.redirect_uri = "https://www.google.be/"
-		self.scope = 'user-library-read'
-
-		self.username = "1136425634"
-
-		token = util.prompt_for_user_token(self.username, self.scope, client_id=self.client_id,
-										   client_secret=self.client_secret,
-										   redirect_uri=self.redirect_uri)
-		if token:
-			self.sp = spotipy.Spotify(auth=token)
-		else:
-			logging.ERROR("Cannot login with this user account")
-			sys.exit(0)
-
-		self.master = master
-
-		self.currentWidgets = []
-		self.bgcolor = "#2ECC71"
-
-		master.configure(background=self.bgcolor)
-
-		master.title("Spotipy GUI")
-		master.geometry("800x800")
-
-		self.queryframe = LabelFrame(master, text="Query", borderwidth=3, padx=5, pady=5, relief=GROOVE, bg=self.bgcolor)
-		self.queryframe.grid(row=0, column=0, sticky=E + W + N + S)
-
-		self.resultframe = LabelFrame(master, text="Result", borderwidth=3, padx=5, pady=5, relief=GROOVE, bg=self.bgcolor)
-		self.resultframe.grid(row=1, column=0, sticky=E + W + N + S)
-
-		self.controlframe = LabelFrame(master, text="Play", borderwidth=1, padx=5, pady=5, relief=GROOVE, bg=self.bgcolor)
-		self.controlframe.grid(row=0, column=1, sticky=E + N)
-
-		welcome = Label(self.queryframe, text="Welcome.", bg=self.bgcolor)
-		welcome.grid(sticky=W, column=0, row=0)
-
-		lbl = Label(self.queryframe, text="Enter an album URI:", bg=self.bgcolor)
-		lbl.grid(sticky=W, column=0, row=1)
-
-		txt = Entry(self.queryframe, width=30, bg="#229954")
-		txt.grid(sticky=W, column=0, row=2)
-		txt.focus()
-		self.txt = txt
-
-		btn = Button(self.queryframe, text="Enter", command=self.search, bg=self.bgcolor)
-		btn.configure(background="grey")
-		btn.grid(sticky=W, column=0, row=3)
-
-		self.errorText = Label(self.resultframe, text="Invalid URI", bg=self.bgcolor)
-
-		playbtn = Button(self.controlframe, text="play", bg=self.bgcolor)
-		playbtn.grid(sticky=W, column=0, row=1)
-
-		self.master.bind('<Return>', self.search2)
-		self.master.bind('<Command-a>', self.selectall)
-		#self.master.bind('<Control-a>', self.selectall) Will work for Windows?
-
-
-	def search(self):
-		for w in self.currentWidgets:
-			w.destroy()
-		album_uri = self.txt.get()
-		#album_uri = "spotify:album:07RagZtMuBbLBnaWJbD52h"
-		try:
-			if album_uri != "spotify:track:4wupMhFWAQzP8CGROC1D2r":
-				results = self.sp.album(album_id=album_uri)
-				logging.info("Showing info for following album:\n%s" % results)
-				self.currentWidgets = showresults(self.resultframe, results)
-			else:
-				results = self.sp.audio_features(tracks=["spotify:track:4wupMhFWAQzP8CGROC1D2r"])
-				#results = self.sp.audio_analysis(track_id="spotify:track:4wupMhFWAQzP8CGROC1D2r")
-				print results
-		except spotipy.SpotifyException:
-			self.errorText.pack()
-
-	def search2(self, Event):
-		self.search()
-
-	def selectall(self, Event):
-		print('event.widget.get():', Event.widget.get())
-
-		# select text
-		Event.widget.select_range(0, 'end')
-		# move cursor to the end
-		Event.widget.icursor('end')
-
-if __name__ == "__main__":
-	logging.basicConfig(level=logging.INFO)
-	window = Tk()
-	spotipyApp = App(window)
-	window.mainloop()
-
-#spotify:album:7lOKvvK9dCayXwR7925yk4
+try:
+	if source == "artist":
+		results = sp.artist_albums(uri)
+		print results
+	else:
+		results = sp.user_playlist(user=username, playlist_id=uri)
+		print results
+except spotipy.SpotifyException:
+	logging.error("Something went wrong. Please check the URI. Also, an internet connection is required.")
--- spotipyGUI.py
+++ spotipyGUI.py
@@ -1,44 +1,140 @@
+import spotipy
+import spotipy.util as util
+import logging
+import io
+import urllib
+from PIL import ImageTk
+from PIL import Image as imImage
+from Tkinter import *
+from helpers import *
 
 
-currentWidgets = []
-bgcolor = "#2ECC71"
+def showresults(window, results):
 
-master.configure(background=bgcolor)
+	res = QueryResult(results)
+	currAlbumWidgets = []
 
-master.title("Spotipy GUI")
-master.geometry("800x800")
+	lbl = Label(window, text=res.parseInfo())
+	lbl.configure(bg="grey")
+	lbl.grid(column=0, row=4)
+	currAlbumWidgets.append(lbl)
 
-queryframe = LabelFrame(master, text="Query", borderwidth=3, padx=5, pady=5, relief=GROOVE, bg=bgcolor)
-queryframe.grid(row=0, column=0, sticky=E + W + N + S)
+	id = 0
+	for name in res.tracks["name"]:
+		id += 1
+		lbltext = "%d: %s" % (id, name)
+		label = Label(window, text=lbltext)
+		label.configure(bg="grey")
+		label.grid(sticky=W, column=0, row=id + 4)
+		currAlbumWidgets.append(label)
 
-resultframe = LabelFrame(master, text="Result", borderwidth=3, padx=5, pady=5, relief=GROOVE, bg=bgcolor)
-resultframe.grid(row=1, column=0, sticky=E + W + N + S)
+	logging.info("URL is %s" % res.coverurl)
+	raw_data = urllib.urlopen(res.coverurl).read()
+	im = imImage.open(io.BytesIO(raw_data))
+	im = im.resize((350, 350), imImage.ANTIALIAS)
+	image = ImageTk.PhotoImage(im)
+	imageLabel = Label(window, image=image)
+	imageLabel.image = image
+	imageLabel.grid(column=0, row=id + 5)
+	currAlbumWidgets.append(imageLabel)
 
-controlframe = LabelFrame(master, text="Play", borderwidth=1d, padx=5, pady=5, relief=GROOVE, bg=bgcolor)
-controlframe.grid(row=0, column=1, sticky=E + N)
+	return currAlbumWidgets
 
-welcome = Label(queryframe, text="Welcome.", bg=bgcolor)
-welcome.grid(sticky=W, column=0, row=0)
+class App:
 
-lbl = Label(queryframe, text="Enter an album URI:", bg=bgcolor)
-lbl.grid(sticky=W, column=0, row=1)
+	def __init__(self, master):
+		self.client_id = "580cb72fd1364d10aa8bd0f8d4bf5c32"
+		self.client_secret = "fa391051c65748b7b7b35d7d6d4faa93"
+		self.redirect_uri = "https://www.google.be/"
+		self.scope = 'user-library-read'
 
-txt = Entry(queryframe, width=30, bg="#229954")
-txt.grid(sticky=W, column=0, row=2)
-txt.focus()
-txt = txt
+		self.username = "1136425634"
 
-btn = Button(queryframe, text="Enter", command=search, bg=bgcolor)
-btn.configure(background="grey")
-btn.grid(sticky=W, column=0, row=3)
+		token = util.prompt_for_user_token(self.username, self.scope, client_id=self.client_id,
+										   client_secret=self.client_secret,
+										   redirect_uri=self.redirect_uri)
+		if token:
+			self.sp = spotipy.Spotify(auth=token)
+		else:
+			logging.ERROR("Cannot login with this user account")
+			sys.exit(0)
 
-errorText = Label(resultframe, text="Invalid URI", bg=bgcolor)
+		self.master = master
 
-playbtn = Button(controlframe, text="play", bg=bgcolor)
-playbtn.grid(sticky=W, column=0, row=1)
+		self.currentWidgets = []
+		self.bgcolor = "#2ECC71"
 
-master.bind('<Return>', search2)
-master.bind('<Command-a>', selectall)
-#master.bind('<Control-a>', selectall) Will work for Windows?
+		master.configure(background=self.bgcolor)
+
+		master.title("Spotipy GUI")
+		master.geometry("800x800")
+
+		self.queryframe = LabelFrame(master, text="Query", borderwidth=3, padx=5, pady=5, relief=GROOVE, bg=self.bgcolor)
+		self.queryframe.grid(row=0, column=0, sticky=E + W + N + S)
+
+		self.resultframe = LabelFrame(master, text="Result", borderwidth=3, padx=5, pady=5, relief=GROOVE, bg=self.bgcolor)
+		self.resultframe.grid(row=1, column=0, sticky=E + W + N + S)
+
+		self.controlframe = LabelFrame(master, text="Play", borderwidth=1, padx=5, pady=5, relief=GROOVE, bg=self.bgcolor)
+		self.controlframe.grid(row=0, column=1, sticky=E + N)
+
+		welcome = Label(self.queryframe, text="Welcome.", bg=self.bgcolor)
+		welcome.grid(sticky=W, column=0, row=0)
+
+		lbl = Label(self.queryframe, text="Enter an album URI:", bg=self.bgcolor)
+		lbl.grid(sticky=W, column=0, row=1)
+
+		txt = Entry(self.queryframe, width=30, bg="#229954")
+		txt.grid(sticky=W, column=0, row=2)
+		txt.focus()
+		self.txt = txt
+
+		btn = Button(self.queryframe, text="Enter", command=self.search, bg=self.bgcolor)
+		btn.configure(background="grey")
+		btn.grid(sticky=W, column=0, row=3)
+
+		self.errorText = Label(self.resultframe, text="Invalid URI", bg=self.bgcolor)
+
+		playbtn = Button(self.controlframe, text="play", bg=self.bgcolor)
+		playbtn.grid(sticky=W, column=0, row=1)
+
+		self.master.bind('<Return>', self.search2)
+		self.master.bind('<Command-a>', self.selectall)
+		#self.master.bind('<Control-a>', self.selectall) Will work for Windows?
 
 
+	def search(self):
+		for w in self.currentWidgets:
+			w.destroy()
+		album_uri = self.txt.get()
+		#album_uri = "spotify:album:07RagZtMuBbLBnaWJbD52h"
+		try:
+			if album_uri != "spotify:track:4wupMhFWAQzP8CGROC1D2r":
+				results = self.sp.album(album_id=album_uri)
+				logging.info("Showing info for following album:\n%s" % results)
+				self.currentWidgets = showresults(self.resultframe, results)
+			else:
+				results = self.sp.audio_features(tracks=["spotify:track:4wupMhFWAQzP8CGROC1D2r"])
+				#results = self.sp.audio_analysis(track_id="spotify:track:4wupMhFWAQzP8CGROC1D2r")
+				print results
+		except spotipy.SpotifyException:
+			self.errorText.pack()
+
+	def search2(self, Event):
+		self.search()
+
+	def selectall(self, Event):
+		print('event.widget.get():', Event.widget.get())
+
+		# select text
+		Event.widget.select_range(0, 'end')
+		# move cursor to the end
+		Event.widget.icursor('end')
+
+if __name__ == "__main__":
+	logging.basicConfig(level=logging.INFO)
+	window = Tk()
+	spotipyApp = App(window)
+	window.mainloop()
+
+#spotify:album:7lOKvvK9dCayXwR7925yk4
